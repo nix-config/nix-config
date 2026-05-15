203dc443579002b5766e886c8af7d0998b676705
骑士姬 <2067834160@qq.com>
2026-05-15 23:38:15 +0800

    chore: 更新 linux 内核

--- vars/kernelTypes/lqx-latest.nix
+++ vars/kernelTypes/lqx-latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "lqx";
-    version = "7.0.6";
+    version = "7.0.7";
     modDirVersion = "${version}-${name}1";
     url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-    sha256 = "sha256-ErTKBIlICKr83X2ULiKltMibu++lWfLMCuRN4dBZNy8=";
+    sha256 = "sha256-ynWfmFIYADVPwFEHHXAkfQPDB2h7G8u4nQxscd8umeo=";
   };
 in
 kernel
