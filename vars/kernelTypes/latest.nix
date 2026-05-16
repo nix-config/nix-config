5b307d3a97721dddcdb074eae089c25f6f7e4180
骑士姬 <2067834160@qq.com>
2026-05-16 10:27:24 +0800

    chore: 更新 linux 内核

--- vars/kernelTypes/latest.nix
+++ vars/kernelTypes/latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "latest";
-    version = "7.0.7";
+    version = "7.0.8";
     modDirVersion = "${version}";
     url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${modDirVersion}.tar.xz";
-    sha256 = "sha256-yOH+hqOq/y3m90ATg/lZKDUQw4n/hMFatxHI+86yXfQ=";
+    sha256 = "sha256-GUWvsiyfT3x42XEhDzu7fesJ9djEGji/rHct4l9tyyI=";
   };
 in
 kernel
--- vars/kernelTypes/lqx-latest.nix
+++ vars/kernelTypes/lqx-latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "lqx";
-    version = "7.0.7";
+    version = "7.0.8";
     modDirVersion = "${version}-${name}1";
     url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-    sha256 = "sha256-ynWfmFIYADVPwFEHHXAkfQPDB2h7G8u4nQxscd8umeo=";
+    sha256 = "sha256-ad585eS7P/b9oKhDipyStXWg+BFCLRJbMEt9+KbvBJQ=";
   };
 in
 kernel
