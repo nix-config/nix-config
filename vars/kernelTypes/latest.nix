e8c13b3862b478d9b84f8316d5e156a60e978544
骑士姬 <2067834160@qq.com>
2026-05-18 16:07:47 +0800

    chore: 更新 linux 内核

--- vars/kernelTypes/latest.nix
+++ vars/kernelTypes/latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "latest";
-    version = "7.0.8";
+    version = "7.0.9";
     modDirVersion = "${version}";
     url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${modDirVersion}.tar.xz";
-    sha256 = "sha256-GUWvsiyfT3x42XEhDzu7fesJ9djEGji/rHct4l9tyyI=";
+    sha256 = "sha256-rAes33bPRiHMUYeiZwJwoaaZUzyKayJeSHjEFq2D8cQ=";
   };
 in
 kernel
