405a4f0332683c6f04819ae4621bd6424537e983
骑士姬 <2067834160@qq.com>
2026-05-20 21:22:37 +0800

    chore: 添加 comfyui 模型和自定义节点

--- modules/nixos/service/comfyui.nix
+++ modules/nixos/service/comfyui.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   opts,
   inputs,
   ...
@@ -7,6 +8,19 @@
 let
   cfg = opts.service.comfyui or { };
   finallyEnable = cfg.enable or false;
+  # 定义模型
+  realesrgan-x4plus-anime-6b = pkgs.fetchResource {
+    name = "RealESRGAN_x4plus_anime_6B.pth";
+    url = "https://huggingface.co/spaces/Marne/Real-ESRGAN/blob/main/RealESRGAN_x4plus_anime_6B.pth";
+    hash = "sha256-+knS/IugSoGGnRKT7x2FesZnF+ii4WX8+uRQzsdirCo=";
+    passthru.comfyui.installPaths = [ "upscale_models" ];
+  };
+  diffusion-pytorch-model-promax = pkgs.fetchResource {
+    name = "diffusion_pytorch_model_promax.safetensors";
+    url = "https://huggingface.co/xinsir/controlnet-union-sdxl-1.0/resolve/main/diffusion_pytorch_model_promax.safetensors";
+    hash = "sha256-n64uUMtDG/y+BYIrWewiKN9UXvJ/cR3qiUnp9O2ffNw=";
+    passthru.comfyui.installPaths = [ "controlnet" ];
+  };
 in
 {
   imports = [
@@ -17,6 +31,15 @@ in
       enable = true;
       host = "0.0.0.0";
       acceleration = "cuda";
+      customNodes = with pkgs.comfyuiPackages; [
+        comfyui-rgthree
+        comfyui-crystools
+        comfyui-pythongosssss-custom-scripts
+      ];
+      models = [
+        realesrgan-x4plus-anime-6b
+        diffusion-pytorch-model-promax
+      ];
     };
   };
 }
