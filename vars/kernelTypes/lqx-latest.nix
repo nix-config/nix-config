9bc878c0cfa2f2a512c825e75b258d82f9710daf
骑士姬 <2067834160@qq.com>
2026-05-24 10:56:25 +0800

    chore: 更新 linux 内核

--- vars/kernelTypes/lqx-latest.nix
+++ vars/kernelTypes/lqx-latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "lqx";
-    version = "7.0.9";
+    version = "7.0.10";
     modDirVersion = "${version}-${name}1";
     url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-    sha256 = "sha256-f/c8fvBpDsWn1ZvGFkVBxlr6oHcHlNPUPiU0hymVFRw=";
+    sha256 = "sha256-wWDE7Wk43/itkauq9RSmw89qwuckDsfd1I+V2iFx1Hg=";
   };
 in
 kernel
--- vars/kernelTypes/zen-latest.nix
+++ vars/kernelTypes/zen-latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "zen";
-    version = "7.0.9";
+    version = "7.0.10";
     modDirVersion = "${version}-${name}1";
     url = "https://github.com/zen-kernel/zen-kernel/archive/refs/tags/v${modDirVersion}.tar.gz";
-    sha256 = "sha256-YCEMOk2ssMzSQ21VazXLGHHOULVrT8drj8DSxuV5MqI=";
+    sha256 = "sha256-85wjUNtWc4UqGughRmcSPpIJDktxkj3Z6e4nTLaeqmg=";
   };
 in
 kernel
