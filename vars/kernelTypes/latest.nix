62d273393e27a965b9d7e5e92722e957e024297f
骑士姬 <2067834160@qq.com>
2026-05-24 10:56:25 +0800

    chore: 更新 linux 内核

--- vars/kernelTypes/latest.nix
+++ vars/kernelTypes/latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "latest";
-    version = "7.0.9";
+    version = "7.0.10";
     modDirVersion = "${version}";
     url = "https://cdn.kernel.org/pub/linux/kernel/v7.x/linux-${modDirVersion}.tar.xz";
-    sha256 = "sha256-rAes33bPRiHMUYeiZwJwoaaZUzyKayJeSHjEFq2D8cQ=";
+    sha256 = "sha256-CUl362LCDj0ZOf6BqSlYofmH8zlEblMvqGljsoBOMtw=";
   };
 in
 kernel
--- vars/kernelTypes/xanmod-latest.nix
+++ vars/kernelTypes/xanmod-latest.nix
@@ -4,10 +4,10 @@
 let
   kernel = rec {
     name = "xanmod";
-    version = "7.0.9";
+    version = "7.0.10";
     modDirVersion = "${version}-${name}1";
     url = "https://gitlab.com/xanmod/linux/-/archive/${modDirVersion}.tar.bz2";
-    sha256 = "sha256-QYTXvja9SIcV9Za2Lse8eH09Z4DU+GF+xUN3NiEnUd0=";
+    sha256 = "sha256-Gl+++EfcbCKV+UIJbOpHWN5VhkZTKvA2woxDBonUNOM=";
   };
 in
 kernel
