cc32042808951f7a614fd44e99e0ad084dc30545
骑士姬 <2067834160@qq.com>
2026-05-28 22:57:03 +0800

    fix: 移除已在 nixpkgs 修复的 patch

--- modules/home/tool/lutris.nix
+++ modules/home/tool/lutris.nix
@@ -7,20 +7,11 @@
 let
   cfg = opts.tool.lutris or { };
   finallyEnable = cfg.enable or false && ((opts.desktop.type or "") != "");
-  # 禁用 openldap 在 i686 上的测试
-  pkgs-patched = pkgs.extend (
-    final: prev: {
-      openldap = prev.openldap.overrideAttrs (_: {
-        doCheck = !prev.stdenv.hostPlatform.isi686;
-      });
-    }
-  );
 in
 {
   config = lib.mkIf finallyEnable {
     programs.lutris = {
       enable = true;
-      package = pkgs-patched.lutris;
       # 为 lutris 配合 umu-launcher 使用而添加的 proton 软件包列表
       protonPackages = with pkgs; [
         dwproton-bin
