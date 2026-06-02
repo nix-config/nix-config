f23ed529b3f92bca26faa5a3cece0a584a58d39d
骑士姬 <2067834160@qq.com>
2026-06-02 10:33:45 +0800

    fix: 运行时调整

--- vars/kernelTypes/cachyos.nix
+++ vars/kernelTypes/cachyos.nix
@@ -2,13 +2,17 @@ inputs:
 let
   cachyos-kernel = inputs.nix-cachyos-kernel.legacyPackages.x86_64-linux;
   kernel = {
-    bore-lto-v3 = {
-      name = "cachyos-bore-lto-v3";
-      packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-x86_64-v3;
+    bmq-lto = {
+      name = "cachyos-bmq-lto";
+      packages = cachyos-kernel.linuxPackages-cachyos-bmq-lto;
     };
     server-lto = {
       name = "cachyos-server-lto";
       packages = cachyos-kernel.linuxPackages-cachyos-server-lto;
+    };
+    bore-lto-v3 = {
+      name = "cachyos-bore-lto-v3";
+      packages = cachyos-kernel.linuxPackages-cachyos-bore-lto-x86_64-v3;
     };
   };
 in
