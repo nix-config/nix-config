5c549fc5cd34b0c553263abfc5094b9f6c018f5e
骑士姬 <2067834160@qq.com>
2026-06-11 21:31:37 +0800

    chore: 移除 opencode-flake

--- modules/home/cli/opencode.nix
+++ modules/home/cli/opencode.nix
@@ -1,23 +1,16 @@
 {
   lib,
-  pkgs,
   opts,
-  inputs,
   ...
 }:
 let
   cfg = opts.cli.opencode or { };
   finallyEnable = cfg.enable or false;
-  opencode-flake = inputs.opencode-flake.packages.${pkgs.stdenv.hostPlatform.system};
-  opencode = opencode-flake.opencode;
-  opencode-avx = opencode-flake.opencode-avx;
-  package = if cfg.avx or true then opencode-avx else opencode;
 in
 {
   config = lib.mkIf finallyEnable {
     programs.opencode = {
       enable = true;
-      inherit package;
     };
   };
 }
