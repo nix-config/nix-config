d67b2f05376d14bf5c90e919585b6a26c1f9e40d
骑士姬 <2067834160@qq.com>
2026-06-13 16:08:26 +0800

    refactor: 将 searxng nixos-container 化

--- modules/nixos/service/searxng.nix
+++ modules/nixos/service/searxng.nix
@@ -8,43 +8,58 @@ let
   cfg = opts.service.searxng or { };
   enableSopsNix = opts.service.sops-nix.enable or false;
   finallyEnable = cfg.enable or false && enableSopsNix;
+  firewall = cfg.firewall or { enable = false; };
 in
 {
   config = lib.mkIf finallyEnable {
     sops.secrets."searxng.env" = {
       sopsFile = ../../../secrets/searxng.env;
       format = "dotenv";
-      owner = "searx";
-      group = "searx";
+      owner = "root";
+      group = "root";
       mode = "0400";
     };
-    services.searx = {
-      enable = true;
-      # 使用 Redis 限流/缓存/会话存储
-      redisCreateLocally = true;
-      # 使用 uWSGI 运行生产模式
-      configureUwsgi = true;
-      uwsgiConfig = {
-        socket = "/run/searx/searx.sock";
-        chmod-socket = "660";
-        # 直连 HTTP 端口 (不依赖 Nginx)
-        http = ":8888";
-      };
-      environmentFile = config.sops.secrets."searxng.env".path;
-      settings = {
-        # 支持的请求格式
-        search.formats = [
-          "html"
-          "json"
-        ];
-        server = {
-          # 允许局域网访问
-          bind_address = "0.0.0.0";
-          # 是否限流
-          limiter = false;
-          # 引用环境变量
-          secret_key = "$SEARXNG_SECRET";
+    containers.searxng = {
+      autoStart = true;
+      privateNetwork = false;
+      bindMounts = {
+        "/run/secrets/searxng.env" = {
+          hostPath = config.sops.secrets."searxng.env".path;
+          isReadOnly = true;
         };
+      };
+      config = { ... }: {
+        services.searx = {
+          enable = true;
+          # 使用 Redis 限流/缓存/会话存储
+          redisCreateLocally = true;
+          # 使用 uWSGI 运行生产模式
+          configureUwsgi = true;
+          uwsgiConfig = {
+            socket = "/run/searx/searx.sock";
+            chmod-socket = "660";
+            # 直连 HTTP 端口 (不依赖 Nginx)
+            http = ":8888";
+          };
+          environmentFile = "/run/secrets/searxng.env";
+          settings = {
+            # 支持的请求格式
+            search.formats = [
+              "html"
+              "json"
+            ];
+            server = {
+              # 允许局域网访问
+              bind_address = "0.0.0.0";
+              # 是否限流
+              limiter = false;
+              # 引用环境变量
+              secret_key = "$SEARXNG_SECRET";
+            };
+          };
+        };
+        networking.firewall = firewall;
+        system.stateVersion = opts.stateVersion;
       };
     };
   };
