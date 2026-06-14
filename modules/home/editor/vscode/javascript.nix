3ce544e3dfbd11ae3f8b8544d27b87c8ce36e3ed
骑士姬 <2067834160@qq.com>
2026-06-14 21:43:54 +0800

    chore: 更新 vscode 扩展

--- modules/home/editor/vscode/javascript.nix
+++ modules/home/editor/vscode/javascript.nix
@@ -25,8 +25,8 @@ in
           # Ripple 框架辅支持
           name = "ripple-ts-vscode-plugin";
           publisher = "Ripple-TS";
-          version = "2.0.33";
-          sha256 = "sha256-+qLRAvkQhW47IyhklvWimQfuNBUDVLyu7mwPWu+uiAo=";
+          version = "2.0.34";
+          sha256 = "sha256-G5aTb92wIVvAObMc5MKY4L7BB8tB6e9+W1KAgxa13Oo=";
         }
       ];
   };
