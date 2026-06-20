de2071a7d966d8eacd5c11ac5f2d56923e891809
骑士姬 <2067834160@qq.com>
2026-06-20 23:27:36 +0800

    fix: 修复错误的配置文件路径

--- modules/home/desktop/dms/dms.nix
+++ modules/home/desktop/dms/dms.nix
@@ -7,13 +7,13 @@
 let
   cfg = opts.desktop.dms or { };
   finallyEnable = cfg.enable or false && ((opts.desktop.type or "") != "");
-  configPath = "${opts.nixConfigPath}/modules/config";
+  configPath = "${opts.nixConfigPath}/modules/home/desktop/dms/config";
 in
 {
   config = lib.mkIf finallyEnable {
     home.file = {
       ".config/DankMaterialShell" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${configPath}/modules/home/desktop/dms/config";
+        source = config.lib.file.mkOutOfStoreSymlink configPath;
         force = true;
       };
     };
--- modules/home/editor/vscode/vscode.nix
+++ modules/home/editor/vscode/vscode.nix
@@ -7,18 +7,18 @@
 let
   cfg = opts.editor.vscode or { };
   finallyEnable = cfg.enable or false && ((opts.desktop.type or "") != "");
-  configPath = "${opts.nixConfigPath}/modules/config";
+  configPath = "${opts.nixConfigPath}/modules/home/editor/vscode/config";
 in
 {
   config = lib.mkIf finallyEnable {
     programs.vscode.enable = true;
     home.file = {
       ".vscode/argv.json" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${configPath}/modules/home/editor/vscode/config/argv.json";
+        source = config.lib.file.mkOutOfStoreSymlink "${configPath}/argv.json";
         force = true;
       };
       ".config/Code/User/settings.json" = {
-        source = config.lib.file.mkOutOfStoreSymlink "${configPath}/modules/home/editor/vscode/config/settings.json";
+        source = config.lib.file.mkOutOfStoreSymlink "${configPath}/settings.json";
         force = true;
       };
     };
