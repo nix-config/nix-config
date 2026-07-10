eb6630182f932e01d2dbd72c02b8691870044cf0
骑士姬 <2067834160@qq.com>
2026-07-10 22:31:25 +0800

    chore: 更新锁文件

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
     nixvim = {
       url = "github:nix-community/nixvim/main";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
       url = "github:Mic92/sops-nix/master";
