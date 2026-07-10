b06667cb5756ff059d54bafa54468a3d29918038
骑士姬 <2067834160@qq.com>
2026-07-10 08:51:55 +0800

    chore: 更新锁文件

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
     # self.submodules = true;
     nixpkgs.url = "github:NixOS/nixpkgs/master";
     nixpkgs-nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # nixpkgs-knightfemale.url = "github:knightfemale/nixpkgs/master";
-    # nixpkgs-knightfemale.url = ./repositories/NixOS/nixpkgs;
     nixpkgs-knightfemale-byterover-cli.url = "github:knightfemale/nixpkgs/byterover-cli";
     nur = {
       url = "github:nix-community/NUR/main";
