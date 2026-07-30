4480c8b132be2b89d0db4a65c4651c3973d2421a
骑士姬 <2067834160@qq.com>
2026-07-30 23:00:08 +0800

    chore: 更新锁文件

--- modules/home/cli/mcp/mcp-nixos.nix
+++ modules/home/cli/mcp/mcp-nixos.nix
@@ -7,16 +7,12 @@
 let
   inherit (opts.cli.mcp) enabledMcps;
   enableModule = (lib.elem "mcp-nixos" enabledMcps) || (lib.elem "all" enabledMcps);
-  # TODO
-  mcp-nixos = pkgs.mcp-nixos.overridePythonAttrs (old: {
-    disabledTests = (old.disabledTests or [ ]) ++ [ "test_read_text_file" ];
-  });
 in
 {
   config = lib.mkIf enableModule {
     programs.mcp.servers."mcp-nixos" = {
       enabled = true;
-      command = "${lib.getExe mcp-nixos}";
+      command = "${lib.getExe pkgs.mcp-nixos}";
       type = "local";
     };
   };
