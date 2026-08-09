079de28ec89c4134b220115be6f12e99d6cd6d25
骑士姬 <2067834160@qq.com>
2026-08-09 11:50:55 +0800

    fix: 取消自定义 options 维持低耦合性

--- modules/home/tool/lutris.nix
+++ modules/home/tool/lutris.nix
@@ -15,8 +15,9 @@ in
       enable = true;
       # 与 programs.steam 共享同一实例, 避免双 Steam 冲突
       steamPackage = steamCfg.package;
-      # 与 programs.steam 共享 Proton 列表
-      protonPackages = steamCfg.protonPackages;
+      protonPackages = with pkgs; [
+        dwproton-bin
+      ];
       extraPackages = with pkgs; [
         mangohud
         umu-launcher
